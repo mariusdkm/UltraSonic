47d595690daa3124ba851c61a6d4f326d78361fa
Qther <dtaclanlegion@gmail.com>
2021-08-08 11:44:36 +0800

    Safety check

--- src/main/java/io/github/mariusdkm/ultrasonic/pathing/Cache.java
+++ src/main/java/io/github/mariusdkm/ultrasonic/pathing/Cache.java
@@ -1,7 +1,10 @@
 package io.github.mariusdkm.ultrasonic.pathing;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -21,8 +24,17 @@ public class Cache {
     }
 
     public static boolean isWalkable(World world, BlockPos pos) {
-        return !world.getBlockState(pos).getCollisionShape(world, pos).isEmpty() &&
-                world.getBlockState(pos.up()).isAir() &&
-                world.getBlockState(pos.up(2)).isAir();
+        return !world.getBlockState(pos).getCollisionShape(world, pos).isEmpty() && isSafe(world.getBlockState(pos)) &&
+                world.getBlockState(pos.up()).getCollisionShape(world, pos).isEmpty() && isSafe(world.getBlockState(pos.up())) &&
+                world.getBlockState(pos.up(2)).getCollisionShape(world, pos).isEmpty() && isSafe(world.getBlockState(pos.up(2)));
+    }
+
+    public static boolean isSafe(BlockState state) {
+        /**
+         * Check that the block does not override onEntityCollision like {@link net.minecraft.block.WitherRoseBlock#onEntityCollision}
+         **/
+        return Arrays.stream(state.getBlock().getClass().getDeclaredMethods()).filter(method ->
+                method.getName().equals("method_26180") || method.getName().equals("onEntityCollision")
+        ).findFirst().isEmpty();
     }
 }
